6ee6e80c08e47ced1688bb0cfad7583da3498544
Kevin Su <kevinsu@outlook.co.nz>
2020-03-18 16:52:39 +1100

    Updates

---
+++ precache-manifest.9df650938c7c9454694e2199930b3983.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c3aa4917babb8ea100007302dcbbf3",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "9b1c05ab0944a02d99c0",
+    "url": "/weather/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6aaf0aad272e77256e6e",
+    "url": "/weather/static/css/main.49be3e8e.chunk.css"
+  },
+  {
+    "revision": "9b1c05ab0944a02d99c0",
+    "url": "/weather/static/js/2.70b52f84.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather/static/js/2.70b52f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aaf0aad272e77256e6e",
+    "url": "/weather/static/js/main.68b08b29.chunk.js"
+  },
+  {
+    "revision": "21baee1ee3fded119a76",
+    "url": "/weather/static/js/runtime-main.6859beea.js"
+  },
+  {
+    "revision": "a8239081690d13299f4fb28568a6c7c0",
+    "url": "/weather/static/media/background.a8239081.jpg"
+  }
+]);
